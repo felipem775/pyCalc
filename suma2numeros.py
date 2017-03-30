3f83af0de2447dffe267832a36e53e95ee068846
Felipe Maza <felipe.maza@unican.es>
2017-03-30 10:34:02 +0200

    Añadida validación de que se trata de dos parámetros y que éstos son números

--- suma2numeros.py
+++ suma2numeros.py
@@ -1,2 +1,13 @@
 import sys
-print(int(sys.argv[1]) + int(sys.argv[2]))
+import re
+if len(sys.argv) != 3:
+    print("El script debe recibir dos parámetros a sumar")
+
+elif  re.match("^[+-]?\d(>?\.\d+)?$", sys.argv[1]) is None:
+    print("El primer parámetro no es un número")
+
+elif  re.match("^[+-]?\d(>?\.\d+)?$", sys.argv[2]) is None:
+    print("El segundo parámetro no es un número")
+
+else:
+    print(int(sys.argv[1]) + int(sys.argv[2]))
